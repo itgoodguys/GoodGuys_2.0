ba1ac8084273377e3c73d1b0a5055382fc91d9b4
Milos Rancic <milos.rancic@madetight.com>
2024-06-28 09:06:19 +0200

    branding, ppc, seo, discovery pages

--- src/js/brandingDesign.js
+++ src/js/brandingDesign.js
@@ -1,3 +1,10 @@
 
 // CSS import
 import '../css/branding-design.scss'
+
+// JavaScript import
+import './components/testimonialSlider.js'
+import './components/breadcrumbs.js'
+import './components/servicesCard.js'
+import './components/discovery.js'
+import './components/faq.js'
--- src/js/webflowSeo.js
+++ src/js/webflowSeo.js
@@ -1,3 +1,11 @@
 
 // CSS import
 import '../css/webflow-seo.scss'
+
+
+// JavaScript import
+import './components/breadcrumbs.js'
+import './components/testimonialSlider.js'
+import './components/discovery.js'
+import './components/faq.js'
+
