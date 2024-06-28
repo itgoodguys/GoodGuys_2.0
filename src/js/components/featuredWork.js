c1d3c94c420bee94360fbcea2f82dcd0b6d568f8
Milos Rancic <milos.rancic@madetight.com>
2024-06-28 16:28:28 +0200

    featured work

---
+++ src/js/components/featuredWork.js
@@ -0,0 +1,8 @@
+// swiper docs
+// https://swiperjs.com/get-started
+
+// import Swiper JS
+import Swiper from 'swiper';
+
+// import Swiper styles
+import 'swiper/css';
--- src/js/home.js
+++ src/js/home.js
@@ -6,4 +6,5 @@ import '../css/home.scss'
 import './components/servicesSlider.js'
 import './components/testimonialSlider.js'
 import './components/blogList.js'
+import './components/featuredWork.js'
 
