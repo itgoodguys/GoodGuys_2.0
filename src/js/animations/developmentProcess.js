e7e32ae1dda1e27d912a14b96581831d2d27a3d6
Milos Rancic <milos.rancic@madetight.com>
2024-08-09 11:09:12 +0200

    delimicno radi

--- src/js/animations/developmentProcess.js
+++ src/js/animations/developmentProcess.js
@@ -1,57 +1,62 @@
-// import gsap from 'gsap';
-// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// $(".process").each(function () {
-//   let wrap = $(this);
-//   let track = $(this).find(".process_track");
-//   let inner = $(this).find(".scroll_horizontal_inner");
-//   let mediaImage = $(this).find(".process_media-image");
-//   let firstSlide = $(this).find(".first-slide");
+$(".process").each(function () {
+  let wrap = $(this);
+  let track = $(this).find(".process_track");
 
-//   // Set section height
-//   function setScrollDistance() {
-//     wrap.css("height", "calc(" + track.outerWidth() + "px + 100vh)");
-//   }
-//   setScrollDistance();
-//   ScrollTrigger.refresh();
-//   window.addEventListener("resize", setScrollDistance);
+  // Set section height
+  function setScrollDistance() {
+    wrap.css("height", "calc(" + track.outerWidth() + "px + 100vh)");
+  }
+  setScrollDistance();
+  ScrollTrigger.refresh();
+  window.addEventListener("resize", setScrollDistance);
 
-//   // Create main horizontal scroll timeline
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: wrap,
-//       start: "top top",
-//       end: "bottom bottom",
-//       scrub: true,
-//     },
-//     defaults: { ease: "none" },
-//   });
+  // Create main horizontal scroll timeline
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: wrap,
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+    },
+    defaults: { ease: "none" },
+  });
 
-//   tl.to(track, { xPercent: -100 });
+  tl.to(track, { xPercent: -100 });
 
-//   function containerLeft() {
-//     return inner.offset().left + "px";
-//   }
-//   // get container right position
-//   function containerRight() {
-//     return (inner.offset().left + inner.innerWidth()) + "px";
-//   }
+  let mediaImage = document.querySelector(".process_media-image");
+  let mediaContainer = document.querySelector(".process_media");
 
-//     // Create the second timeline for scaling the image
-//     let tl2 = gsap.timeline({
-//         scrollTrigger: {
-//           trigger: firstSlide,
-//           start: "left " + containerLeft(),
-//           end: "right " + containerRight(),
-//           scrub: true,
-//         },
-//       });
-    
-//       // Scale the image from 33% to 100% and back
-//       tl2.fromTo(mediaImage, { scale: 0.33 }, { scale: 1, ease: "none" })
-//          .to(mediaImage, { scale: 0.33, ease: "none" });
+  // Create a timeline for each process_screen element
+  wrap.find(".process_screen").each(function (index) {
+    let screen = $(this);
 
+    // Create a timeline for mediaImage's width animation
+    let screenTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: screen,
+        start: "center center",
+        end: "left+=50% center", // Adjust the end point as needed
+        scrub: true,
+        duration: 1,
+      },
+      defaults: { ease: "none" },
+    });
 
-// });
+    if (index % 2 === 0) {
+      console.log('even')
+      // Even index: maxWidth 33%, justify-content flex-start
+      screenTl.to(mediaImage, { maxWidth: "33%" })
+              .to(mediaContainer, { justifyContent: "flex-start" }, 0);
+    } else {
+      console.log('odd')
+      // Odd index: maxWidth 100%, justify-content flex-end
+      screenTl.to(mediaImage, { maxWidth: "100%" })
+              .to(mediaContainer, { justifyContent: "flex-end" }, 0);
+    }
+  });
+});
