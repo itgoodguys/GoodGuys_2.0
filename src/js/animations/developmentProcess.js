7183f950ae8041eccdeb6d063d6674b2c65fc2b0
Milos-GoodGuy <milos@goodguys.nu>
2024-09-16 12:50:00 +0200

    development process slider edit

--- src/js/animations/developmentProcess.js
+++ src/js/animations/developmentProcess.js
@@ -1,26 +1,21 @@
 import '../../css/animations/_development-process.scss';
 
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// swiper docs
+// https://swiperjs.com/get-started
 
-gsap.registerPlugin(ScrollTrigger);
+// import Swiper JS
+import Swiper from 'swiper';
+import { Scrollbar } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
-// Select the container, panels, and elements to update
-const horizontalScroll = document.querySelector('.process_content');
-const panels = document.querySelectorAll('.process_slide');
-const process = document.querySelector('.process');
-const pageWrapper = document.querySelector('.page-wrapper');
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+
 const slideNumbersTracks = document.querySelectorAll('.process_slide-numbers-track');
 
-// Calculate the total width of the horizontal scroll content
-const scrollWidth = horizontalScroll.scrollWidth;
-const viewportWidth = process.offsetWidth;
-
-// console.log('scrollWidth', scrollWidth);
-// console.log('viewportWidth', viewportWidth);
-// console.log('panels.length', panels.length);
-
-// Function to update classes based on the active slide
+// Function to update classes based on the active slide (so we can change the numbers on the slides)
 function updateActiveSlideClasses(slideIndex) {
   slideNumbersTracks.forEach(track => {
     track.className = 'process_slide-numbers-track'; // Reset classes
@@ -28,39 +23,51 @@ function updateActiveSlideClasses(slideIndex) {
   });
 }
 
-// Create the horizontal scroll animation
-gsap.to(horizontalScroll, {
-  x: () => -(scrollWidth - viewportWidth) + "px",
-  ease: "none",
-  scrollTrigger: {
-    trigger: '.process',
-    start: "top top",
-    end: () => `+=${scrollWidth - viewportWidth}`,
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-    snap: {
-      snapTo: 1 / (panels.length - 1), // 0, 0.20, 0.4, 0.6, 0.8 based on number of panels
-      snapTo: 1 / (panels.length - 1), //  based on number of panels
-      duration: { min: 0.2, max: 0.6 }, // Duration range for the snapping
-      ease: "power1.inOut" // Easing function for snapping
+
+let servicesSlider;
+
+function initSwiper() {
+  servicesSlider = new Swiper('.process', {
+    modules: [Scrollbar, Navigation],
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 0,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+      draggable: true,
+      dragSize: 'auto',
+      snapOnRelease: true
     },
-    onUpdate: self => {
-      const currentProgress = self.progress;
-      const slideIndex = Math.round(currentProgress * (panels.length - 1));
+    navigation: {
+      nextEl: '.process_navigation-next',
+      prevEl: '.process_navigation-prev',
+    },
+    on: {
+      slideChangeTransitionStart: function () {
+        // Set navigation opacity to 0 when slide starts changing
+        document.querySelector('.process_navigation-next').style.opacity = '0';
+        document.querySelector('.process_navigation-prev').style.opacity = '0';
+      },
+      slideChangeTransitionEnd: function () {
+        // Set navigation opacity back to 1 when slide stops changing
+        document.querySelector('.process_navigation-next').style.opacity = '1';
+        document.querySelector('.process_navigation-prev').style.opacity = '1';
 
-      // Update the slide number track classes based on the current slide
-      updateActiveSlideClasses(slideIndex);
-
-      // Update page-wrapper class for the last slide
-      // const lastPanelIndex = panels.length - 1;
-      // const lastPanelStart = lastPanelIndex / panels.length;
-      
-      // if (currentProgress >= lastPanelStart) {
-      //   pageWrapper.classList.add('dark-background');
-      // } else {
-      //   pageWrapper.classList.remove('dark-background');
-      // }
+      },
+      slideChange: function () {
+        // Update the slide number track classes based on the current slide
+        updateActiveSlideClasses(this.activeIndex);
+      }
     }
-  }
-});
+  });
+}
+
+// Initialize Swiper on load
+initSwiper();
+
+// Re-initialize Swiper on resize
+window.addEventListener('resize', initSwiper);
+
+
+
