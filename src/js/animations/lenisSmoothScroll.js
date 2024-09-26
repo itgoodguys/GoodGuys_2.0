a27c6dd56f4791c873d7b01c5c9dbbbf60d00c12
Milos Rancic <milos.rancic@madetight.com>
2024-09-26 15:24:59 +0200

    lenis whelmultiplier edit

--- src/js/animations/lenisSmoothScroll.js
+++ src/js/animations/lenisSmoothScroll.js
@@ -5,7 +5,7 @@ import Lenis from 'lenis';
 
 const lenis = new Lenis({
   lerp: 0.1,
-  wheelMultiplier: 0.9,
+  wheelMultiplier: 0.7,
   infinite: false,
   guestureOrientation: 'vertical',
   normalizeWheel: false,
