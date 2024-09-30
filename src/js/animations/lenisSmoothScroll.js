49ab1c8a5205ff281b33b4b3d9ff2133e11f2393
Milos-GoodGuy <milos@goodguys.nu>
2024-09-30 08:20:07 +0200

    edit lenis scroll distance

--- src/js/animations/lenisSmoothScroll.js
+++ src/js/animations/lenisSmoothScroll.js
@@ -4,8 +4,8 @@ import '../../css/animations/_lenis-smooth-scroll.scss'
 import Lenis from 'lenis';
 
 const lenis = new Lenis({
-  lerp: 0.3,
-  wheelMultiplier: 0.7,
+  lerp: 0.3, // scroll smoothness
+  wheelMultiplier: 0.9, // scroll distance
   infinite: false,
   guestureOrientation: 'vertical',
   normalizeWheel: false,
